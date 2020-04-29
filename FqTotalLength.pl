ce43260e98f86bc7ce60987019eade4965f3b6f6
Jeffrey C.H. Yu <itsjeffreyy76@gmail.com>
2020-04-29 11:08:14 +0800

    Update multiple input function

--- FqTotalLength.pl
+++ FqTotalLength.pl
@@ -1,14 +1,28 @@
 #!/usr/bin/perl -w
 # usage : FqTotalLength.pl seqence.fq
 # writer: Jeffrey Yu
-open(IN,"<$ARGV[0]")||die "open file $ARGV[0]:$!\n";
-my $len=0;
-while(<IN>){
-	if ($_=~/^@/){
-	 	my $seq=<IN>; chomp $seq;
-		$len+=length($seq);
-		<IN>;<IN>;
-	}
+use strict;
+use Data::Dumper;
+
+my $total_leng=0;
+foreach(@ARGV){
+	$total_leng+=&SingleFqLeng($_);
 }
-close IN;
-print "Fastq $ARGV[0] Total= $len\n";
+print "Total length: $total_leng\n";
+
+############################################################
+sub SingleFqLeng(){
+	my $fq=shift(@_);
+	open(IN,"<$fq")||die "open file $fq:$!\n";
+	my $len=0;
+	while(<IN>){
+		if ($_=~/^@/){
+		 	my $seq=<IN>; chomp $seq;
+			$len+=length($seq);
+			<IN>;<IN>;
+		}
+	}
+	close IN;
+	print "$fq: $len\n";
+	return($len);
+}
