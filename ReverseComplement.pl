da71a1f4f2415cb15eaa0dba73a35df86f06b5f0
Jeffrey C.H. Yu <itsjeffreyy76@gmail.com>
2020-07-15 12:15:56 +0800

    modify ReversComplement.pl and timestamp.pl

--- ReverseComplement.pl
+++ ReverseComplement.pl
@@ -3,10 +3,49 @@
 # writer:Jeffreyy Yu
 use strict;
 use Data::Dumper;
+use Getopt::Long;
 
-my $seq=$ARGV[0];
-my $rcseq=RC($seq);
-print "$rcseq\n";
+my $fa;
+GetOptions(
+	"fa=s" => \$fa,
+);
+
+if (! $fa){
+	my $seq=$ARGV[0];
+	my $rcseq= &RC($seq);
+	print "$rcseq\n";
+}elsif ($fa){
+	my $check = &CheckFaFormat($fa);
+	if($check == 0){
+		print "ERR: Probably not Fasta format!\n";
+		exit;
+	}elsif($check == 1){
+		my %title_seq=();
+		my @title=();
+		open(FaIN,"<$fa")|| die "open $fa: $!\n";
+		while(<FaIN>){
+			chomp;
+			if($_=~/^>(.+)/){
+				push(@title,$1);
+			}else{
+				$title_seq{$title[-1]}.=$_;
+			}
+		}
+		close FaIN;
+		
+		# fasta file
+		my ($rc_fa_fn,$ext)=$fa=~/(.+)((?:.fasta|.fa|.fna))/;
+		$rc_fa_fn.="_rc".$ext;
+		print "MSG: Generate RC fasta file $rc_fa_fn\n";
+
+		open(FaOUT, "> $rc_fa_fn") || die "ERR: Can not write $rc_fa_fn\n";
+		foreach my $t (@title){
+			print FaOUT ">$t\n";
+			print FaOUT &RC($title_seq{$t})."\n";
+		}
+		close FaOUT;
+	}
+}
 
 #################################################################
 sub RC{
@@ -15,4 +54,16 @@ sub RC{
 	$read = reverse $read;
 	$read=~tr/ATCGatcg/TAGCtagc/;
 	return $read;
+}
+
+sub CheckFaFormat{
+	my $faf=shift @_;
+	open(FaIN,"<$faf") || die "open $fa: $!\n";
+	my $line=<FaIN>;
+	close FaIN;
+	if($line=~/^>/){
+		return 1;
+	}else{
+		return 0;
+	}
 }
--- timestamp.pl
+++ timestamp.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl -w
 use strict;
-###
-my $datetimestamp = localtime(time);
-print "$datetimestamp\n";
+
+print &Timestamp."\n";
+
 ###
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
 # time
+my $time = sprintf("%04d-%02d-%02d %02d:%02d:%02d",$year+1900, $mon+1, $mday,$hour,$min,$sec);
+print "$time\n";
+
 my $timestamp = sprintf("%02d:%02d:%02d", $hour, $min, $sec);
 print "$timestamp\n";
 # date
@@ -13,3 +16,11 @@ my @months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
 my @days = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
 my $ryear=$year+1900;
 print "$days[$wday] $months[$mon] $mday $ryear\n";
+
+
+
+#########################################################
+sub Timestamp{
+	my $datetimestamp = localtime(time);
+	return $datetimestamp;
+}
