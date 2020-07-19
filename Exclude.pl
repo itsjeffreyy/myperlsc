328536563ba2ada54201822854d5370afa2ddbf3
Jeffrey C.H. Yu <itsjeffreyy76@gmail.com>
2020-07-19 12:52:06 +0800

    add Exclude.pl

---
+++ Exclude.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+use strict;
+
+# exclude list
+my @ex=();
+open(INex,"<$ARGV[1]")|| die "open $ARGV[1]: $!\n";
+while(<INex>){
+	chomp;
+	my @a=split("\t",$_);
+	push(@ex,$a[0]);
+}
+close INex;
+
+open(IN1,"<$ARGV[0]")|| die "open $ARGV[0]: $!\n";
+while(<IN1>){
+	chomp;
+	my $check=0;
+	foreach my $e (@ex){
+		if($_=~/$e/){
+			$check =1;
+		}
+	}
+	if($check == 0){
+		print "$_\n";
+	}
+}
+close IN1;
