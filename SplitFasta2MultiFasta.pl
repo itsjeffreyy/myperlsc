68425d9c27e1bee2a5faa01a49e38f45f1cbac64
Jeffreyy C.H. Yu <itsjeffreyy76@gmail.com>
2021-03-22 14:43:31 +0800

    Add split script!

---
+++ SplitFasta2MultiFasta.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -w
+use strict;
+use Data::Dumper;
+use Getopt::Long;
+
+# variable
+my $split=1;
+my $fa_f=$ARGV[0];
+
+# extract the file head
+my ($fa_h)=$fa_f=~/(.+)(?:fa|fasta)/;
+
+# options
+GetOptions(
+	"split|s=i" => \$split,
+);
+
+# check format
+open(IN,"<$fa_f")|| die "open Fasta $fa_f:$!\n";
+my $l1=<IN>; chomp $l1;
+my $l2=<IN>; chomp $l2;
+if($l1!~/^>/ ){
+	print "ERR: $fa_f not a Fasta file.\n";
+	exit;
+}
+close IN;
+
+# check line number
+my $line_num=`wc -l $fa_f`; chomp $line_num;
+if($line%2 != 0){
+	print "ERR: $fa_f not a Fasta file.\n";
+	exit;
+}
+# get the total read numbers
+my $read_num=$line_num/2;
+
+# set read numbers in one file
+my $read_num_split_fa=int($read_num/$split);
+
+# open fasta
+my $file_num=1;
+my $reads_s=0;
+open(IN,"<$fa_f")|| die "open Fasta $fa_f:$!\n";
+while(<IN>){
+	my $l1=$_; chomp $l1;
+	my $l2=<IN>; chomp $l2;
+
+	if($reads_s == $read_num_split_fa && $file_num < $split){
+		$reads_s=0;
+		close OUT;
+		$file_num++;
+	}
+	if($reads_s==0){
+	}
+
+	print OUT "$l1\n$l2\n";
+	$reads_s++;
+}
+close IN;
+close OUT;
