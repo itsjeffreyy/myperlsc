afe2746030e41c94176fc25f3342f0b1881c399e
Jeffrey, Chun-Hui Yu <itsjeffreyy76@gmail.com>
2023-01-06 11:48:57 +0800

    modify FaLength.pl

--- FaLength.pl
+++ FaLength.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w 
 # writer: Jeffreyy Yu
-# Uasge: FaLength.pl file.fasta output
+# Uasge: EvaluateFaLength.pl file.fasta output
 use strict;
 use Data::Dumper;
 
@@ -13,7 +13,8 @@ while(<IN>){
 	chomp $_;
 	if($_=~/^>(\S+)/){
 		$i++;
-		$id="$1\-$i";	
+		#$id="$1\-$i";	
+		$id="$1";	
 	}else{
 		$sq{$id}.=$_;
 	}
@@ -24,12 +25,6 @@ foreach (keys %sq){
 	$sl{$_}=length($sq{$_});
 }
 
-open(OUT,">unsort.stat");
-foreach my $s(keys %sl){
-	print OUT "$s\t$sl{$s}\n";
-}
-close OUT;
-
 my $output="";
 if (defined $ARGV[1]){
 	$output=$ARGV[0];
@@ -37,5 +32,10 @@ if (defined $ARGV[1]){
 	$output="$ARGV[0].lengstat";
 }
 
-`sort -k2 -n ./unsort.stat > $output `;
-`rm unsort.stat`;
+open(OUT,">$output");
+foreach my $s (sort {$sl{$a} <=> $sl{$b}} (keys %sl)){
+	print OUT "$s\t$sl{$s}\n";
+}
+close OUT;
+
+
